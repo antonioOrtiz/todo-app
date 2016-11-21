f6c2375f35cabd651f4f7b486cc693311a181f6e
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2016-11-21 12:52:14 -0500

    redrawList function completed

--- app/_js/app.js
+++ app/_js/app.js
@@ -1,26 +1,41 @@
 (function(window, document) {
     'use strict';
 
-    var doc = document;
+    var doc = document, todoListItems = [];
+
+    function redrawList() {
+        // body... 
+        var i, 
+            list = doc.getElementById('todo-list'), 
+            len = todoListItems.length;
+            list.innerHTML = '';
+
+            for (i=0; i < len; i++){
+                var item = document.createElement('li');
+                item.appendChild(
+                    doc.createTextNode(todoListItems[i].value)
+                );
+                list.appendChild(item);
+            } 
+    }
 
     window.addEventListener('load', windowLoadHandler, false);
 
+
     function newTodoKeyPressHandler(event) {
         // body... 
-           
-              var todoField = doc.getElementById('new-todo'),
-                  list = doc.getElementById('todo-list'),
-                  item = doc.createElement('li');
+
+        var todoField = doc.getElementById('new-todo'),
+            list = doc.getElementById('todo-list'),
+            item = doc.createElement('li');
 
         if (event.keyCode === 13) {
             // statement
+
+            item.appendChild(doc.createTextNode(todoField.value));
+            list.appendChild(item);
+            todoField.value = '';
             console.log('We have a new Todo');
-          
-
-
-                item.appendChild(doc.createTextNode(todoField.value));
-                list.appendChild(item);
-                todoField.value = '';
 
 
         }
