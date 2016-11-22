546da2b7e0bcbd25f8690d1dbbba09bb147900c7
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2016-11-22 10:08:28 -0500

    added checkbox handler

--- app/_js/app.js
+++ app/_js/app.js
@@ -1,49 +1,102 @@
-(function(window, document) {
-    'use strict';
+(function() {
 
-    var doc = document, todoListItems = [];
+    var todoListItems = [];
 
-    function redrawList() {
-        // body... 
-        var i, 
-            list = doc.getElementById('todo-list'), 
-            len = todoListItems.length;
-            list.innerHTML = '';
-
-            for (i=0; i < len; i++){
-                var item = document.createElement('li');
-                item.appendChild(
-                    doc.createTextNode(todoListItems[i].value)
-                );
-                list.appendChild(item);
-            } 
+    function TodoItem(title, completed) {
+        this.title = title;
+        this.completed = completed;
     }
 
-    window.addEventListener('load', windowLoadHandler, false);
+    function redrawList() {
+        var i;
+        var list = document.getElementById('todo-list');
+        var len = todoListItems.length;
+        list.innerHTML = "";
+        for (i = 0; i < len; i++) {
+            var todo = todoListItems[i];
+            var item = document.createElement("li");
+            if (todo.completed) {
+                item.className += "completed"
+            }
 
+            // checkbox
 
-    function newTodoKeyPressHandler(event) {
-        // body... 
+            var checkbox = document.createElement('input');
+            checkbox.className = "toggle";
+            checkbox.type = "checkbox";
+            checkbox.addEventListener('change', checkboxChangeHandler);
+            checkbox.checked = todo.completed;
+            checkbox.setAttribute('data-todo-id', i);
 
-        var todoField = doc.getElementById('new-todo'),
-            list = doc.getElementById('todo-list'),
-            item = doc.createElement('li');
+            // label
 
-        if (event.keyCode === 13) {
-            // statement
+            var label = document.createElement('label');
+            label.appendChild(document.createTextNode(todo.title));
 
-            item.appendChild(doc.createTextNode(todoField.value));
+            // div wrapper
+
+            var divDisplay = document.createElement('div');
+            divDisplay.className = "view";
+            divDisplay.appendChild(checkbox);
+            divDisplay.appendChild(label);
+
+            item.appendChild(divDisplay);
+
             list.appendChild(item);
-            todoField.value = '';
-            console.log('We have a new Todo');
-
-
         }
     }
 
-    function windowLoadHandler() {
-        // body... 
-        doc.getElementById('new-todo').addEventListener('keypress', newTodoKeyPressHandler, false);
+    function addToList(title) {
+        var todo = new TodoItem(title, false);
+        todoListItems.push(todo);
+        saveList();
     }
 
-})(window, document);
+    function migrateData() {
+        var i, l;
+        for (i = 0, l = todoListItems.length; i < l; i++) {
+            item = todoListItems[i];
+            if (typeof(item) === 'string') {
+                todoListItems[i] = new TodoItem(item, false);
+            }
+        }
+    }
+
+    function reloadList(item) {
+        var stored = localStorage.getItem('todo-list');
+        if (stored) {
+            todoListItems = JSON.parse(stored);
+            migrateData();
+        }
+        redrawList();
+    }
+
+    function saveList() {
+        localStorage.setItem('todo-list', JSON.stringify(todoListItems));
+    }
+
+    function checkboxChangeHandler(event) {
+        var checkbox = event.target;
+        var index = checkbox.getAttribute('data-todo-id');
+        var todo = todoListItems[index];
+        todo.completed = checkbox.checked;
+        saveList();
+        redrawList();
+    }
+
+    function newTodoKeyPressHandler(event) {
+        if (event.keyCode === 13) {
+            var todoField = document.getElementById('new-todo');
+            addToList(todoField.value);
+
+            redrawList();
+            todoField.value = "";
+        }
+    }
+    window.addEventListener('load', windowLoadHandler, false);
+
+    function windowLoadHandler() {
+        reloadList();
+        document.getElementById('new-todo').addEventListener('keypress', newTodoKeyPressHandler, false);
+    }
+}());
