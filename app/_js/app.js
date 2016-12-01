106fa8865d52a3360f75f8628a0bbc3dd7cca7a1
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2016-12-01 18:20:56 -0500

    app.js now includes a unique identify function to create unique IDs

--- app/_js/app.js
+++ app/_js/app.js
@@ -1,103 +1,101 @@
 (function() {
-
     var todoListItems = [],
         doc = document,
         todoapp = doc.getElementById('todoapp'),
         addTodoLink = doc.getElementById('add-todo');
 
-
-    // function getUuid () {
-    //     // body... 
-    //     var i, random, uuid = '';
-
-    //     for(var i = 0, i < 32; i++){
-    //         random = Math.rnadom() * 16 | 0;
-    //         if (i === 8 || i === 12 || i === 16 || i === 20) {
-    //             // statement
-    //             uuid += '-';
-    //         } 
-    //         uuid +=
-    //     }
-
-    // }
+    function getUuid() {
+        var i, random,
+            uuid = '';
+        for (i = 0; i < 32; i++) {
+            random = Math.random() * 16 | 0;
+            if (i === 8 || i === 12 || i === 16 || i === 20) {
+                uuid += '-';
+            }
+            uuid += (i === 12 ? 4 : (i === 16 ? (random & 3 | 8) : random)).toString(16);
+        }
+        return uuid;
+    }
 
     function TodoItem(title, completed) {
         this.title = title;
         this.completed = completed;
+        this.id = getUuid();
     }
 
     function redrawList() {
         var i;
-        var list = doc.getElementById('todo-list');
+        var list = document.getElementById('todo-list');
         var len = todoListItems.length;
         list.innerHTML = "";
         for (i = 0; i < len; i++) {
             var todo = todoListItems[i];
-            var item = doc.createElement("li");
-            item.id = "li_" + i;
+            var item = document.createElement("li");
+            item.id = "li_" + todo.id;
             if (todo.completed) {
-                item.className += "completed";
+                item.className += "completed"
             }
 
             // checkbox
 
-            var checkbox = doc.createElement('input');
+            var checkbox = document.createElement('input');
             checkbox.className = "toggle";
             checkbox.type = "checkbox";
             checkbox.addEventListener('change', checkboxChangeHandler);
             checkbox.checked = todo.completed;
-            checkbox.setAttribute('data-todo-id', i);
+            checkbox.setAttribute('data-todo-id', todo.id);
 
             // label
 
-            var label = doc.createElement('label');
-            label.appendChild(doc.createTextNode(todo.title));
+            var label = document.createElement('label');
+            label.appendChild(document.createTextNode(todo.title));
             label.addEventListener('dblclick', editItemHandler);
-            label.setAttribute('data-todo-id', i);
+            label.setAttribute('data-todo-id', todo.id);
 
+            // delete button
+            deleteButton = document.createElement('button');
+            deleteButton.className = 'destroy';
+            deleteButton.setAttribute('data-todo-id', todo.id);
+            deleteButton.addEventListener('click', deleteClickHandler);
 
             // div wrapper
-            var divDisplay = doc.createElement('div');
+
+            var divDisplay = document.createElement('div');
             divDisplay.className = "view";
             divDisplay.appendChild(checkbox);
             divDisplay.appendChild(label);
+            divDisplay.appendChild(deleteButton);
 
             item.appendChild(divDisplay);
 
             list.appendChild(item);
-
-            // delete button
-            deleteButton = doc.createElement('button');
-            deleteButton.className = 'destroy';
-            deleteButton.setAttribute('data-todo-id', i);
-            deleteButton.addEventListener('click', deleteClickHandler);
-
-            divDisplay.appendChild(deleteButton);
         }
         addTodoLink.classList.add('hidden');
-
-
-
     }
 
-    function deleteTodo(index) {
-        todoListItems.splice(index, 1);
-        saveList();
-        redrawList();
+    function editTodo(id, text) {
+        var todo = getTodoById(id);
+        if (todo) {
+            todo.title = text;
+            saveList();
+            redrawList();
+        }
     }
 
-    function editTodo(index, text) {
-        todoListItems[index].title = text;
-        saveList();
-        redrawList();
+    function deleteTodo(id) {
+        var index = getTodoIndexById(id);
+        if (index > -1) {
+            todoListItems.splice(index, 1);
+            saveList();
+            redrawList();
+        }
     }
 
     function deleteClickHandler(event) {
-        var button = event.target,
-            index = button.getAttribute('data-todo-id');
-        deleteTodo(index);
+        var button = event.target;
+        var id = button.getAttribute('data-todo-id');
+        deleteTodo(id);
     }
-
 
     function addToList(title) {
         var todo = new TodoItem(title, false);
@@ -111,6 +109,9 @@
             item = todoListItems[i];
             if (typeof(item) === 'string') {
                 todoListItems[i] = new TodoItem(item, false);
+            }
+            if (typeof(item.id) === 'undefined') {
+                todoListItems[i] = new TodoItem(item.title, item.completed);
             }
         }
     }
@@ -140,7 +141,22 @@
             // Too bad, no localStorage for us
             console.log('We can\'t seem to use localStorage because it is not available in this browser');
         }
+    }
 
+    function getTodoById(id) {
+        var i, l;
+        for (i = 0, l = todoListItems.length; i < l; i++) {
+            if (todoListItems[i].id == id) return todoListItems[i];
+        }
+        return null;
+    }
+
+    function getTodoIndexById(id) {
+        var i, l;
+        for (i = 0, l = todoListItems.length; i < l; i++) {
+            if (todoListItems[i].id == id) return i;
+        }
+        return -1;
     }
 
     function saveList() {
@@ -148,9 +164,9 @@
     }
 
     function checkboxChangeHandler(event) {
-        var checkbox = event.target,
-            index = checkbox.getAttribute('data-todo-id'),
-            todo = todoListItems[index];
+        var checkbox = event.target;
+        var id = checkbox.getAttribute('data-todo-id');
+        var todo = getTodoById(id)
         todo.completed = checkbox.checked;
         saveList();
         redrawList();
@@ -158,12 +174,12 @@
 
     function newTodoKeyPressHandler(event) {
         if (event.keyCode === 13) {
-            var todoField = doc.getElementById('new-todo'),
-                text = todoField.value.trim();
+            var todoField = document.getElementById('new-todo');
+            var text = todoField.value.trim();
             if (text !== '') {
                 addToList(todoField.value);
                 redrawList();
-                todoField.value = '';
+                todoField.value = "";
             }
         }
     }
@@ -173,72 +189,56 @@
             // statement
             var input = event.target,
                 text = input.value.trim(),
-                index = input.getAttribute('data-todo-id');
+                id = input.getAttribute('data-todo-id');
             input.removeEventListener('blur', inputEditItemKeyPressHandler);
-
             if (text === '') {
                 // statement
-                deleteTodo(index);
+                deleteTodo(id);
             } else {
                 // statement
-                editTodo(index, text);
+                editTodo(id, text);
             }
-
         }
     }
 
     function inputEditItemBlurHandler(event) {
-        // body... 
-        var input = event.target,
-            text = input.value.trim(),
-            index = input.getAttribute('data-todo-id');
+        var input = event.target;
+        var text = input.value.trim();
+        var id = input.getAttribute('data-todo-id');
         if (text === '') {
-            // statement
-            deleteTodo(index);
+            deleteTodo(id);
         } else {
-            // statement
-            editTodo(index, text);
+            editTodo(id, text);
         }
     }
 
     function editItemHandler(event) {
-        var label = event.target,
-            index = label.getAttribute('data-todo-id');
-        todo = todoListItems[index];
-        li = doc.getElementById('li_' + index);
-        input = doc.createElement('input');
-
-        input.setAttribute('data-todo-id', index);
-        input.className = 'edit';
+        var label = event.target;
+        var id = label.getAttribute('data-todo-id');
+        var todo = getTodoById(id);
+        var li = document.getElementById('li_' + id)
+        var input = document.createElement('input');
+        input.setAttribute('data-todo-id', id);
+        input.className = "edit";
         input.value = todo.title;
-        input.addEventListener('keypress', inputEditItemKeyPressHandler);
+        input.addEventListener('keypress', inputEditItemKeypressHandler);
         input.addEventListener('blur', inputEditItemBlurHandler);
-
         li.appendChild(input);
-        li.className = 'editing';
+        li.className = "editing";
         input.focus();
     }
 
     function hideTodo() {
         todoapp.parentNode.removeChild(todoapp);
         addTodoLink.classList.remove('hidden');
-
-
     }
-
 
     function addTodo() {
         var parentOfFooter = doc.getElementById('footer').parentNode,
-        footer = doc.getElementById('footer');
+            footer = doc.getElementById('footer');
         parentOfFooter.insertBefore(todoapp, footer);
-
         addTodoLink.classList.add('hidden');
-
     }
-
-
-
-
     window.addEventListener('load', windowLoadHandler, false);
 
     function windowLoadHandler() {
